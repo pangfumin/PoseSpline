b34430c8caf607ae23c4c4794e4d32065725f251
Fumin Pang <pangfumin@gmail.com>
2018-02-01 10:51:25 +0800

    add vectorspaceError , need test

--- include/pose-spline/VectorSplineSampleError.hpp
+++ include/pose-spline/VectorSplineSampleError.hpp
@@ -1,5 +1,5 @@
-#ifndef QUATERNIONSPLINESAMPLEERROR_H
-#define  QUATERNIONSPLINESAMPLEERROR_H
+#ifndef VECTORSPLINESAMPLEERROR_H
+#define  VECTORSPLINESAMPLEERROR_H
 
 #include "pose-spline/QuaternionSpline.hpp"
 #include <ceres/ceres.h>
@@ -8,26 +8,13 @@
 #include "pose-spline/ErrorInterface.hpp"
 
 
-/*
- * QuaternionSplineSampleError
- *
- * A derived Ceres cost functor to compute the Quaternon errer between quaternion measurement and
- * the evaluation value from spline. For cubic bspline, the evaluation at time t is related to four
- * contor points, so this fuctor requires four paramters.
- *
- * To simplified the jacobian, we define error = /hat ominus /meas.
- *
- * @Author Pang Fumin
- */
-
-class QuaternionSplineSampleError: public ceres::SizedCostFunction<3,4,4,4,4>{
+class VectorSplineSampleError: public ceres::SizedCostFunction<3,3,3,3,3>{
 public:
-
     typedef Eigen::Matrix<double, 3, 3> covariance_t;
     typedef covariance_t information_t;
 
-    QuaternionSplineSampleError(double t_meas, Quaternion Q_meas);
-    virtual ~QuaternionSplineSampleError();
+    VectorSplineSampleError(const double& t_meas, const Eigen::Vector3d& V_meas);
+    virtual ~VectorSplineSampleError();
 
     virtual bool Evaluate(double const* const* parameters,
                           double* residuals,
@@ -43,7 +30,7 @@ public:
 private:
 
     double t_meas_;
-    Quaternion Q_Meas_;
+    Eigen::Vector3d V_Meas_;
     mutable information_t information_; ///< The information matrix for this error term.
     mutable information_t squareRootInformation_; ///< The square root information matrix for this error term.
 };

10e89b0cd84f6adaf4aa0dc00333a4077c071ec3
pangfumin <pangfumin@gmail.com>
2020-10-29 16:19:14 +0800

    test 6D pnp

--- test/pnp/test_pnp.cpp
+++ test/pnp/test_pnp.cpp
@@ -64,12 +64,86 @@ private:
 
 };
 
+void T2double(Eigen::Matrix4d& T,double* ptr){
 
-void pnp(Eigen::Matrix4d& T_WC, std::vector<Eigen::Vector3d>& pt3d, std::vector<Eigen::Vector2d>& pt2d_bearing) {
+    Eigen::Vector3d trans = T.topRightCorner(3,1);
+    Eigen::Matrix3d R = T.topLeftCorner(3,3);
+    Eigen::Quaterniond q(R);
+
+    ptr[0] = trans(0);
+    ptr[1] = trans(1);
+    ptr[2] = trans(2);
+    ptr[3] = q.x();
+    ptr[4] = q.y();
+    ptr[5] = q.z();
+    ptr[6] = q.w();
+}
+
+void applyNoise(const Eigen::Matrix4d& Tin,Eigen::Matrix4d& Tout){
+
+
+    Tout.setIdentity();
+
+    Eigen::Vector3d delat_trans = 0.25*Eigen::Matrix<double,3,1>::Random();
+    Eigen::Vector3d delat_rot = 0.10*Eigen::Matrix<double,3,1>::Random();
+
+    Eigen::Quaterniond delat_quat(1.0,delat_rot(0),delat_rot(1),delat_rot(2)) ;
+    delat_quat.normalize();
+
+    Tout.topRightCorner(3,1) = Tin.topRightCorner(3,1) + delat_trans;
+    Tout.topLeftCorner(3,3) = Tin.topLeftCorner(3,3)*delat_quat.toRotationMatrix();
+}
+
+
+
+typedef Eigen::Matrix<double,7,1> Vec7d;
+
+Vec7d T2param(Eigen::Matrix4d T) {
+    Eigen::Matrix3d R = T.topLeftCorner(3,3);
+    Eigen::Quaterniond q(R);
+    Vec7d param;
+    param << T.topRightCorner(3,1), q.x(), q.y(), q.z(), q.w();
+    return param;
+}
+
+Eigen::Matrix4d param2T(Vec7d vec) {
+    Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
+    T.topRightCorner(3,1) = vec.head(3);
+    Eigen::Quaterniond q(vec[6], vec[3], vec[4], vec[5]);
+    T.topLeftCorner(3,3) = q.toRotationMatrix();
+    return T;
+}
+
+
+void pnp(Eigen::Matrix4d& T_WC, std::vector<Eigen::Vector3d>& pt3d, std::vector<Eigen::Vector3d>& pt3d_bearing) {
     ceres::Problem problem;
     PoseLocalParameterization *poseLocalParameter = new PoseLocalParameterization;
+    std::cout << "before OPT : \n" << T_WC << std::endl;
 
-//    for (int pt3d)
+
+    Vec7d  param = T2param(T_WC);
+
+    problem.AddParameterBlock(param.data(), 7,poseLocalParameter);
+    for (int i = 0; i < pt3d.size(); i ++) {
+        ceres::CostFunction* e = new ProjectError(pt3d_bearing[i], pt3d[i]);
+
+        problem.AddResidualBlock(e,NULL, param.data());
+    }
+
+    ceres::Solver::Options options;
+    options.minimizer_progress_to_stdout = true;
+    options.max_solver_time_in_seconds = 30;
+    options.max_num_iterations = 300;
+    options.linear_solver_type = ceres::SPARSE_SCHUR;
+    options.minimizer_progress_to_stdout = true;
+    options.parameter_tolerance = 1e-4;
+    ceres::Solver::Summary summary;
+    ceres::Solve(options, &problem, &summary);
+    std::cout << summary.FullReport() << std::endl;
+
+    T_WC = param2T(param);
+    std::cout << "OPT : \n" << T_WC << std::endl;
+
 }
 
 
@@ -94,6 +168,12 @@ int main(int argc, char** argv){
 ////        std::cout << "gt       : " << pt2d[i].transpose() << std::endl;
 //
 //    }
+
+
+    Eigen::Matrix4d noised_T_WC;
+    applyNoise(T_WC, noised_T_WC);
+    pnp( noised_T_WC, pt3d, pt3d_bearing);
+
 
 
 

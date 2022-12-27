066103ae8f30e654973521c00cc404f9bf77b155
pangfumin <pangfumin@gmail.com>
2022-12-27 11:34:41 +0800

    add some maplab JPL localposeParamters

---
+++ include/PoseSpline/maplab/pose_param_jpl.h
@@ -0,0 +1,23 @@
+#ifndef CERES_ERROR_TERMS_PARAMETERIZATION_POSE_PARAM_JPL_H_
+#define CERES_ERROR_TERMS_PARAMETERIZATION_POSE_PARAM_JPL_H_
+
+#include <Eigen/Core>
+#include <ceres/ceres.h>
+
+#include "PoseSpline/maplab/quaternion_param_jpl.h"
+
+namespace ceres_error_terms {
+
+class JplPoseParameterization : public ceres::ProductParameterization {
+ public:
+  JplPoseParameterization()
+      : ceres::ProductParameterization(
+            new JplQuaternionParameterization,
+            new ceres::IdentityParameterization(3)) {}
+  virtual ~JplPoseParameterization() {}
+};
+
+
+}  // namespace ceres_error_terms
+
+#endif  // CERES_ERROR_TERMS_PARAMETERIZATION_POSE_PARAM_JPL_H_
